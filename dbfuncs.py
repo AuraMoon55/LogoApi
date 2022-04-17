436064048d786d7fd56b1a0113ceb32f359c8b07
Anshul Garg <garganshul553@gmail.com>
2022-04-17 16:06:18 +0530

    Add files via upload

---
+++ dbfuncs.py
@@ -0,0 +1,45 @@
+from logoapi import db
+
+usersdb = db.users
+
+async def save_key(user: int):
+  ALPHA = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+  tat = str(user)
+  ket = []
+  for a in tat:
+    a = int(a)
+    a -= 1
+    dig = ALPHA[int(a)]
+    ket.append(dig)
+  key = ket[::-1]
+  key1 = key[0:5]
+  key2 = key[5:8]
+  key3 = key[8:]
+  new_key = f"{key1}-{key2}-{key3}"
+  await usersdb.insert_one({"id": user, "key": new_key})
+  return {
+    "key": new_key
+  }
+
+async def get_keys():
+  x = await usersdb.find()
+  keys = []
+  for a in x:
+    keys.append(a["key"])
+  return keys
+
+async def check_key(key):
+  x = await get_keys()
+  if key in x:
+    return True
+  else:
+    return False
+
+async def get_user(user):
+  x = await usersdb.find()
+  for a in x:
+    if a["id"] == user:
+      return a
+    else:
+      pass
+  return None
--- logoapi.py
+++ logoapi.py
@@ -1,39 +1,76 @@
-from fastapi import FastAPI, Request
-from modules import *
-from fastapi.responses import FileResponse
+from modules.pater import *
+from dbfuncs import *
 import os
+from flask import Flask, render_template, request
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 CONG = os.path.join(os.getcwd(), "config.json")
 
-app = FastAPI()
-"""
+app = Flask(__name__)
+
 with open(CONG, "r") as data:
   d = json.load(data)
-"""
 
-@app.get("/")
+mongo_client = MongoClient(d['mongo'])
+db = mongo_client.logoapi
+
+
+@app.route("/")
 async def home():
     return {
       "Name": "Logo Api",
       "Version": "Test",
       "Status": "Up",
-      "Help": "https://testlogoapi.herokuapp.com//help",
-      "Authors": "AuraMoon55"
+      "Help": f"{d['name']}/help",
+      "Authors": d["authors"]
     }
 
-@app.get("/help")
+@app.route("/help")
 async def get_help():
-  fn = "name for first line"
+  fn = "text for logo"
   fe = "name for second line"
   return {
     "Name": "Logo Api",
-    "get_logo": f"https://testlogoapi.herokuapp.com//logo?first={fn}&?last={fe}",
-    "Authors": "AuraMoon55"
+    "get logo with preview": f"{d['name']}/logo?text={fn}",
+    "Authors": d['authors'],
+    "get logo link": f"{d['name']}/api/logo?api_key=your key&text={fn}",
+    "get api key": f"{d['name']}/getKey?user_id=telegram user id"
   }
 
-@app.get("/logo/{first_n}")
-async def logo_p(first_n):
-  first = first_n
+@app.route("/logo")
+async def logo_p():
+  first = request.args.get('text')
   logo = await get_logo(first)
-  return FileResponse(logo)
+  return render_template('post.html', logo=logo)
 
+@app.route("/api/logo")
+async def logo_api():
+  first = request.args.get('text')
+  api = request.args.get('api_key')
+  x = await check_key(api)
+  if x:
+    pass
+  else:
+    return {
+      "error": "wrong api key"
+    }
+  logo = await get_logo(first)
+  return {
+    "API": api,
+    "logo": logo
+  }
+
+@app.route("/getKey")
+async def getKey():
+  userid = request.args.get('user_id')
+  user = await get_user(userid)
+  if user:
+    return {
+      "Key": user["key"]
+    }
+  else:
+    key = await save_key(user_id)
+    return {
+      "Key": key["key"]
+    }
